02fe16866bc60a2702cab759ea02fe40d3b899c8
张俊杰 <911175547@qq.com>
2019-08-20 21:00:34 +0800

    上传图片-使用SDK上传图片

--- src/main/java/com/zjj/project/project/controller/FileController.java
+++ src/main/java/com/zjj/project/project/controller/FileController.java
@@ -1,9 +1,16 @@
 package com.zjj.project.project.controller;
 
 import com.zjj.project.project.dto.FileDTO;
+import com.zjj.project.project.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * @author zhangjunjie 2019/8/20
@@ -11,9 +18,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FileController {
+
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @ResponseBody
     @RequestMapping("/file/upload")
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/bulage.png");
